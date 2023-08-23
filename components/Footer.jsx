6442013b376bd4474eaf838a0b079391a2e189ac
Mohd Tehami <officialtehami@gmail.com>
2023-08-23 15:22:11 +0530

    Improved Responsiveness on click in footer
    Signed-off-by: Mohd Tehami <officialtehami@gmail.com>

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="footer-gradient" />
+   <div className="footer-gradient" style={{ left: '50%' }} />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
